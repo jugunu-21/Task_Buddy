35e1a53b367d1fb17f008497440d84a0baf74e67
jugunu-21 <aaruhijugunu@gmail.com>
2025-02-17 22:04:36 +0530

    calender component to pick date

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,18 @@ import Header from "@/components/Header";
 import FiltersAndSearch from "@/components/FiltersAndSearch";
 import { Provider } from 'react-redux';
 import { store } from '@/lib/redux/store';
+import { TodosListTable } from "@/components/todos/todoslist-table";
+import { CardWithForm } from "@/components/todos/add-todos-form";
+
 export default function Home() {
   return (
     <Provider store={store}>
     <div className="min-h-screen ">
+      
       <Header />
       <FiltersAndSearch />
+      <CardWithForm />
+      <TodosListTable />
     </div></Provider>
   );
 }
