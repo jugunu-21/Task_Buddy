9d2a7f4cedd3a32ec3455521175d1eb35fad9238
jugunu-21 <aaruhijugunu@gmail.com>
2025-02-19 20:04:31 +0530

    implement authentication check in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,19 +2,19 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  // const idToken = request.cookies.get('firebase-token');
-  // const isAuthPage = request.nextUrl.pathname.startsWith('/signin');
+  const idToken = request.cookies.get('firebase-token');
+  const isAuthPage = request.nextUrl.pathname.startsWith('/signin');
 
-  // if (isAuthPage) {
-  //   if (idToken) {
-  //     return NextResponse.redirect(new URL('/', request.url));
-  //   }
-  //   return NextResponse.next();
-  // }
+  if (isAuthPage) {
+    if (idToken) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+    return NextResponse.next();
+  }
 
-  // if (!idToken) {
-  //   return NextResponse.redirect(new URL('/signin', request.url));
-  // }
+  if (!idToken) {
+    return NextResponse.redirect(new URL('/signin', request.url));
+  }
 
   return NextResponse.next();
 }
